2521754263e1ed7220d9d247c28b30933709ac95
safeercsfr <safeermohd0009@gmail.com>
2023-08-21 11:53:59 +0530

    committed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import { AuthContext } from "../context/authContext";
 const Home = () => {
   const { currentUser } = useContext(AuthContext);
   const [posts, setPosts] = useState([]);
+  console.log(posts,'===');
   const cat = useLocation().search;
   useEffect(() => {
     const fetchData = async () => {
@@ -28,22 +29,22 @@ const Home = () => {
       ) : (
         <div className="home">
           <div className="posts">
-            {posts.map((post) => (
-              <div className="post" key={post._id}>
+            {posts?.map((post) => (
+              <div className="post" key={post?._id}>
                 <div className="img">
                   <img src={`../upload/${post?.img}`} alt="" />
                 </div>
                 <div className="content">
-                  <Link className="link" to={`/post/${post._id}`}>
-                    <h1>{post.title}</h1>
+                  <Link className="link" to={`/post/${post?._id}`}>
+                    <h1>{post?.title}</h1>
                   </Link>
                   <p>
-                    {post?.author?.username} "{post.title}" on{" "}
-                    {moment(post.createdAt).format("MMMM Do YYYY")}{" "}
+                    {post?.author?.username} "{post?.title}" on{" "}
+                    {moment(post?.createdAt).format("MMMM Do YYYY")}{" "}
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; (💬₂.₂₄<sub>k</sub>)
                   </p>
-                  <p>{post.desc}</p>
-                  <Link className="link" to={`/post/${post._id}`}>
+                  <p>{post?.desc}</p>
+                  <Link className="link" to={`/post/${post?._id}`}>
                     <button>Read more</button>
                   </Link>
                 </div>
